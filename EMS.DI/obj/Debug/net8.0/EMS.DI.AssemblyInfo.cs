728c4c3d53f3a1cb9bd6b73777360f158dfd9d14
bilal.siddiqui@jamipartners.com <bilal.siddiqui@jamipartners.com>
2025-02-24 17:01:14 +0500

    inital commit

--- EMS.DI/obj/Debug/net8.0/EMS.DI.AssemblyInfo.cs
+++ EMS.DI/obj/Debug/net8.0/EMS.DI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.DI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c1cf81039b4be85269c82472d25e6c3c48ad1f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.DI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.DI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
