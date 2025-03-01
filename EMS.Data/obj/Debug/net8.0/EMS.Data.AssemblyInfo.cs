5e7a307e69317237260b1923817c1eb357e2815d
Bilal Ahmed <bilal.siddiqui@jamipartners.com>
2025-03-01 15:22:15 +0500

    git ignore

--- EMS.Data/obj/Debug/net8.0/EMS.Data.AssemblyInfo.cs
+++ EMS.Data/obj/Debug/net8.0/EMS.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79f50238a168dae1842c952c1ff1736c4f13772")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EMS.Repository/obj/Debug/net8.0/EMS.Repository.AssemblyInfo.cs
+++ EMS.Repository/obj/Debug/net8.0/EMS.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f79f50238a168dae1842c952c1ff1736c4f13772")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
