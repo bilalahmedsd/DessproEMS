c223937bfbd361c6f1eb30c930b564a3229df56a
Bilal Ahmed <bilal.siddiqui@jamipartners.com>
2025-03-15 11:58:56 +0500

    update git ignore

--- EMS.Data/Models/EMSContext.cs
+++ EMS.Data/Models/EMSContext.cs
@@ -35,9 +35,9 @@ public partial class EMSContext : DbContext
 
     public virtual DbSet<UserType> UserTypes { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=192.168.15.33,1433;Initial Catalog=EMS;User ID=sa;Password=sql;Encrypt=False");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=192.168.15.33,1433;Initial Catalog=EMS;User ID=sa;Password=sql;Encrypt=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
